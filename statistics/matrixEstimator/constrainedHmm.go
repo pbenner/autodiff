d7b41fa816d5c9b2a21a97a864ca84b8e59c78fc
Philipp Benner <benner@molgen.mpg.de>
2018-05-16 13:01:37 +0200

    2018/05/16-13:01:37 (Linux zoophobie.molgen.mpg.de unknown)

---
+++ statistics/matrixEstimator/constrainedHmm.go
@@ -0,0 +1,61 @@
+/* Copyright (C) 2017 Philipp Benner
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package matrixEstimator
+
+/* -------------------------------------------------------------------------- */
+
+import   "fmt"
+
+import . "github.com/pbenner/autodiff/statistics"
+import   "github.com/pbenner/autodiff/statistics/generic"
+import   "github.com/pbenner/autodiff/statistics/matrixDistribution"
+
+import . "github.com/pbenner/autodiff"
+
+/* -------------------------------------------------------------------------- */
+
+func NewConstrainedHmmEstimator(pi Vector, tr Matrix, stateMap, startStates, finalStates []int, tree generic.HmmNode, estimators []VectorEstimator, epsilon float64, maxSteps int, args... interface{}) (*HmmEstimator, error) {
+  if hmm, err := matrixDistribution.NewConstrainedHmm(pi, tr, stateMap, nil, tree); err != nil {
+    return nil, err
+  } else {
+    if err := hmm.SetStartStates(startStates); err != nil {
+      return nil, err
+    }
+    if err := hmm.SetFinalStates(finalStates); err != nil {
+      return nil, err
+    }
+    if hmm.NEDists() > 0 && len(estimators) != hmm.NEDists() {
+      return nil, fmt.Errorf("invalid number of estimators")
+    }
+    for i, estimator := range estimators {
+      // initialize distribution
+      if hmm.Edist[i] == nil {
+        hmm.Edist[i] = estimator.GetEstimate()
+      }
+    }
+    // initialize estimators with data
+    r := HmmEstimator{}
+    r.hmm1       = hmm.Clone()
+    r.hmm2       = hmm.Clone()
+    r.hmm3       = hmm.Clone()
+    r.estimators = estimators
+    r.epsilon    = epsilon
+    r.maxSteps   = maxSteps
+    r.args       = args
+    return &r, nil
+  }
+}
