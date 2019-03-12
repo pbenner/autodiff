ff99ec27a5160016abc78ddba2f920e1b985a2b8
Philipp Benner <benner@molgen.mpg.de>
2019-03-12 14:18:36 +0100

    2019/03/12-14:18:29 (Linux zoophobie.molgen.mpg.de unknown)

--- statistics/vectorEstimator/normal_stein_test.go
+++ statistics/vectorEstimator/normal_stein_test.go
@@ -42,3 +42,32 @@ func TestNormalStein1(t *testing.T) {
     t.Error("test failed")
   }
 }
+
+func TestNormalStein2(t *testing.T) {
+  si := NewMatrix(BareRealType, 3, 3, []float64{
+    66,  78,  90,
+    78,  93, 108,
+    90, 108, 126})
+  l, H, err := eigensystem.Run(si, eigensystem.Symmetric{true}); if err != nil {
+    t.Error("test failed"); return
+  }
+  l = (NormalSteinEstimator{}).steinEigen(l, 10)
+
+  // compute covariance matrix using l and H
+  for i := 0; i < l.Dim(); i++ {
+    for j := 0; j < l.Dim(); j++ {
+      si.At(i,j).Mul(H.ConstAt(j,i), l.ConstAt(i))
+    }
+  }
+  si.MdotM(H, si)
+
+  v := NullReal()
+  s := NewMatrix(BareRealType, 3, 3, []float64{
+    55.07872, 64.96757,  74.85642,
+    64.96757, 77.44899,  89.93040,
+    74.85642, 89.93040, 105.00439 })
+
+  if v.Mnorm(si.MsubM(si,s)); v.GetValue() > 1e-4 {
+    t.Error("test failed")
+  }
+}
