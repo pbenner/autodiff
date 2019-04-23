b8d6e05158ea729b7f7c117bf91e93f7717f3c94
Philipp Benner <philipp.benner@mis.mpg.de>
2019-04-23 21:29:28 +0200

    2019/04/23-21:29:26 (Linux thalamus unknown)

--- demo/example1/example1.go
+++ demo/example1/example1.go
@@ -25,11 +25,10 @@ import   "github.com/pbenner/autodiff/algorithm/gradientDescent"
 import   "github.com/pbenner/autodiff/algorithm/rprop"
 import . "github.com/pbenner/autodiff/simple"
 
-import   "github.com/gonum/plot"
-import   "github.com/gonum/plot/plotter"
-import   "github.com/gonum/plot/plotutil"
-import   "github.com/gonum/plot/vg"
-
+import   "gonum.org/v1/plot"
+import   "gonum.org/v1/plot/plotter"
+import   "gonum.org/v1/plot/plotutil"
+import   "gonum.org/v1/plot/vg"
 
 /* -------------------------------------------------------------------------- */
 
