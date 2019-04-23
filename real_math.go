1a2b22581d66150c53b2eb99fb583a2e288cf8e1
Philipp Benner <benner@molgen.mpg.de>
2019-04-23 15:07:54 +0200

    2019/04/23-15:07:54 (Linux sirius unknown)

--- real_math.go
+++ real_math.go
@@ -524,8 +524,8 @@ func (r *Real) VdotV(a, b ConstVector) Scalar {
 func (r *Real) Vnorm(a ConstVector) Scalar {
   r.Reset()
   t := NewReal(0.0)
-  for i := 0; i < a.Dim(); i++ {
-    t.Pow(a.ConstAt(i), ConstReal(2.0))
+  for entry := range a.ConstRange() {
+    t.Pow(entry.Value, ConstReal(2.0))
     r.Add(r, t)
   }
   r.Sqrt(r)
