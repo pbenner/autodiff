d8c65fa733ea2cc58f31734f6a203f0760d6bbfc
Philipp Benner <philipp.benner@mis.mpg.de>
2019-04-26 12:57:13 +0200

    2019/04/26-12:57:13 (Linux thalamus unknown)

--- algorithm/saga/saga_test.R
+++ algorithm/saga/saga_test.R
@@ -6,3 +6,9 @@ bc$Cell.size  <- as.numeric(bc$Cell.size)
 bc$Cell.shape <- as.numeric(bc$Cell.shape)
 
 m <- glm(Class ~ Cell.size + Cell.shape, family="binomial", data = bc[1:20,])
+
+## sparsified data
+## -----------------------------------------------------------------------------
+
+bc$Cell.size  <- bc$Cell.size  - 1
+bc$Cell.shape <- bc$Cell.shape - 1
