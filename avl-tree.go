6d8fbd0cac38fa218bcbe55d5af3f4b32bb480cf
Philipp Benner <philipp.benner@mis.mpg.de>
2019-04-28 10:32:45 +0200

    2019/04/28-10:32:44 (Linux thalamus unknown)

--- avl-tree.go
+++ avl-tree.go
@@ -23,6 +23,7 @@ type AvlNode struct {
   Balance int
   Left   *AvlNode
   Right  *AvlNode
+  Parent *AvlNode
 }
 
 type AvlTree struct {
@@ -39,25 +40,25 @@ func NewAvlNode(i int) *AvlNode {
 
 /* -------------------------------------------------------------------------- */
 
-func (obj AvlTree) Emtpy() bool {
+func (obj *AvlTree) Emtpy() bool {
   return obj.Root == nil
 }
 
-func (obj AvlTree) Left() AvlTree {
+func (obj *AvlTree) Left() AvlTree {
   return AvlTree{obj.Root.Left}
 }
 
-func (obj AvlTree) Right() AvlTree {
+func (obj *AvlTree) Right() AvlTree {
   return AvlTree{obj.Root.Right}
 }
 
-func (obj AvlTree) Value() int {
+func (obj *AvlTree) Value() int {
   return obj.Root.Value
 }
 
 /* -------------------------------------------------------------------------- */
 
-func (obj AvlTree) FindNode(i int) *AvlNode {
+func (obj *AvlTree) FindNode(i int) *AvlNode {
   node := obj.Root
   for node != nil {
     switch {
@@ -69,7 +70,7 @@ func (obj AvlTree) FindNode(i int) *AvlNode {
   return nil
 }
 
-func (obj AvlTree) Insert(i int) bool {
+func (obj *AvlTree) Insert(i int) bool {
   if obj.Root == nil {
     obj.Root = NewAvlNode(i)
     return true
@@ -78,14 +79,32 @@ func (obj AvlTree) Insert(i int) bool {
   return ok
 }
 
+func (obj *AvlTree) Iterator() *AvlIterator {
+  return NewAvlIterator(obj.Root)
+}
+
 /* -------------------------------------------------------------------------- */
+
+func (obj *AvlNode) setLeft(node *AvlNode) {
+  obj.Left = node
+  if node != nil {
+    node.Parent = obj
+  }
+}
+
+func (obj *AvlNode) setRight(node *AvlNode) {
+  obj.Right = node
+  if node != nil {
+    node.Parent = obj
+  }
+}
 
 func (obj *AvlNode) insert(i int, parent *AvlNode) (bool, bool) {
   if obj == nil {
     switch {
-    case i  < obj.Value: parent.Left  = NewAvlNode(i)
-    case i  > obj.Value: parent.Right = NewAvlNode(i)
-    case i == obj.Value: return false, true
+    case i  < parent.Value: parent.setLeft (NewAvlNode(i))
+    case i  > parent.Value: parent.setRight(NewAvlNode(i))
+    case i == parent.Value: return false, true
     }
     return true, false
   }
@@ -140,10 +159,10 @@ func (obj *AvlNode) rotateLL() {
   a1 := obj.Left
   a2 := obj.Right
 
-  obj.Left  = a1.Left
-  obj.Right = a1
-  a1 .Left  = a1.Right
-  a1 .Right = a2
+  obj.setLeft (a1.Left)
+  obj.setRight(a1)
+  a1 .setLeft (a1.Right)
+  a1 .setRight(a2)
 
   obj.Value, a1.Value = a1.Value, obj.Value
 
@@ -155,10 +174,10 @@ func (obj *AvlNode) rotateLR() {
   a1 := obj.Left
   a2 :=  a1.Right
 
-  a1 .Right = a2 .Left
-  a2 .Left  = a2 .Right
-  a2 .Right = obj.Right
-  obj.Right = a2
+  a1 .setRight(a2 .Left)
+  a2 .setLeft (a2 .Right)
+  a2 .setRight(obj.Right)
+  obj.setRight(a2)
 
   obj.Value, a2.Value = a2.Value, obj.Value
 
@@ -179,10 +198,10 @@ func (obj *AvlNode) rotateRR() {
   a1 := obj.Right
   a2 := obj.Left
 
-  obj.Right = a1.Right
-  obj.Left  = a1
-  a1 .Right = a1.Left
-  a1 .Left  = a2
+  obj.setRight(a1.Right)
+  obj.setLeft (a1)
+  a1 .setRight(a1.Left)
+  a1 .setLeft (a2)
 
   obj.Value, a1.Value = a1.Value, obj.Value
 
@@ -194,10 +213,10 @@ func (obj *AvlNode) rotateRL() {
   a1 := obj.Right
   a2 :=  a1.Left
 
-  a1 .Left  =  a2.Right
-  a2 .Right =  a2.Left
-  a2 .Left  = obj.Left
-  obj.Left  = a2
+  a1 .setLeft ( a2.Right)
+  a2 .setRight( a2.Left)
+  a2 .setLeft (obj.Left)
+  obj.setLeft (a2)
 
   obj.Value, a2.Value = a2.Value, obj.Value
 
@@ -212,4 +231,55 @@ func (obj *AvlNode) rotateRL() {
     obj.Left .Balance =  0
   }
   obj.Balance = 0
+}
+
+/* -------------------------------------------------------------------------- */
+
+type AvlIterator struct {
+  node *AvlNode
+}
+
+func NewAvlIterator(node *AvlNode) *AvlIterator {
+  for node.Left != nil {
+    node = node.Left
+  }
+  r := AvlIterator{}
+  r.node = node
+  return &r
+}
+
+func (obj *AvlIterator) Get() *AvlNode {
+  return obj.node
+}
+
+func (obj *AvlIterator) Ok() bool {
+  return obj.node != nil
+}
+
+func (obj *AvlIterator) Next() {
+  if obj.node == nil {
+    return
+  }
+  if obj.node.Right != nil {
+    // there is a node to the right where we can go
+    // further down
+    obj.node = obj.node.Right
+    for obj.node.Left != nil {
+      obj.node = obj.node.Left
+    }
+  } else {
+    // there is no node to the right, so we need to go up
+    // => find an ancestor node where this branch is on
+    // the left
+    for obj.node.Parent != nil && obj.node.Parent.Right != nil && obj.node.Parent.Right == obj.node {
+      obj.node = obj.node.Parent
+    }
+    if obj.node.Parent == nil {
+      // we are at the root, stop
+      obj.node = nil
+    } else {
+      // found next node
+      obj.node = obj.node.Parent
+    }
+  }
 }
