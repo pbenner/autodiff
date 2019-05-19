68cb916876d7ac4cff82cb753a440d43b2f9a2d5
Philipp Benner <benner@molgen.mpg.de>
2019-05-19 09:49:44 +0200

    2019/05/19-09:49:44 (Linux zoophobie.molgen.mpg.de unknown)

--- algorithm/saga/saga_dense.gen.h
+++ algorithm/saga/saga_dense.gen.h
@@ -1,14 +1,12 @@
 
-#define                SAGA_NAME Dense
-#define        CONST_SCALAR_NAME ConstReal
-#define              SCALAR_NAME BareReal
-#define              VECTOR_NAME DenseBareRealVector
-#define       SPARSE_VECTOR_NAME DenseBareRealVector
-#define CONST_SPARSE_VECTOR_NAME DenseConstRealVector
+#define         SAGA_NAME Dense
+#define CONST_SCALAR_NAME ConstReal
+#define       SCALAR_NAME BareReal
+#define       VECTOR_NAME DenseBareRealVector
+#define  DATA_VECTOR_NAME DenseConstRealVector
 
-#define                SAGA_TYPE                SAGA_NAME
-#define        CONST_SCALAR_TYPE        CONST_SCALAR_NAME
-#define              SCALAR_TYPE             *SCALAR_NAME
-#define              VECTOR_TYPE              VECTOR_NAME
-#define       SPARSE_VECTOR_TYPE       SPARSE_VECTOR_NAME
-#define CONST_SPARSE_VECTOR_TYPE CONST_SPARSE_VECTOR_NAME
+#define         SAGA_TYPE         SAGA_NAME
+#define CONST_SCALAR_TYPE CONST_SCALAR_NAME
+#define       SCALAR_TYPE      *SCALAR_NAME
+#define       VECTOR_TYPE       VECTOR_NAME
+#define  DATA_VECTOR_TYPE  DATA_VECTOR_NAME
