e5472beec1cb64a08f76279f3755c1ae8a56d691
Philipp Benner <philipp@zeroprobability.org>
2021-12-07 20:02:29 +0100

    2021/12/07-20:02:29 (Linux cray unknown)

--- demo/logisticPerformance/logisticTorch.py
+++ demo/logisticPerformance/logisticTorch.py
@@ -1,16 +1,19 @@
 
+import numpy as np
 import time
 import torch as tr
 
 ## objective function
 ## -----------------------------------------------------------------------------
 
-def rosenbrock(z):
-    a =   1.0
-    b = 100.0
-    x = z[0]
-    y = z[1]
-    return tr.pow((a - x), 2.0) + b*tr.pow(y - x*x, 2.0)
+class Net(tr.nn.Module):
+    def __init__(self, m):
+        super(Net, self).__init__()
+        self.linear1 = tr.nn.Linear(m, 1, bias=False)
+    def forward(self, x):
+        x = self.linear1(x)
+        x = tr.sigmoid(x)
+        return x
 
 ## -----------------------------------------------------------------------------
 
@@ -22,20 +25,29 @@ def measure_time(closure):
 
 ## -----------------------------------------------------------------------------
 
+# import data
+X = tr.from_numpy(np.genfromtxt('logisticData.X.table', delimiter=' ')).float()
+y = tr.from_numpy(np.genfromtxt('logisticData.y.table', delimiter=' ')).float()
+
 # number of steps
-T = 10000
+T = 1000
+m = 100
+
+loss_function = tr. nn.BCELoss()
+net = Net(m)
+net.linear1.weight.data.fill_(0.0)
+# define initial value
+optimizer = tr.optim.Adam(net.parameters())
 
 def test():
-    # define initial value
-    x = tr.tensor([-10.0,10.0], requires_grad=True)
-    optimizer = tr.optim.Adam([x])
     # make T optimization steps
     for i in range(1, T):
         optimizer.zero_grad()
-        y = rosenbrock(x)
-        y.backward()
+        y_hat = tr.flatten(net(X))
+        loss = loss_function(y_hat, y)
+        loss.backward()
         optimizer.step()
-    print(x)
+    print(list(net.parameters()))
 
 ## -----------------------------------------------------------------------------
 
